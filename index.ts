9fd4399bd1ff91b0bdfe8ef072d01beee6c019d1
justin <youngcheol33@gmail.com>
2021-07-01 22:35:23 +0900

    index.ts에서 DB에 데이터 올려주는 구문 다 주석처리해줌. 이제 여기서부터 작업 하면됨!! 이상한거 연습하려면 브랜치 새로 파서 해라 ㅡㅡ

--- index.ts
+++ index.ts
@@ -4,27 +4,27 @@ import { assignments, projects, users } from './data/data';
 const prisma = new PrismaClient()
 
 async function main() {
-    // ... you will write your Prisma Client queries here
-    // // // First - we add user
-    await prisma.user.createMany({
-        data: users
-    })
+    // // ... you will write your Prisma Client queries here
+    // // // // First - we add user
+    // await prisma.user.createMany({
+    //     data: users
+    // })
   
     const allUsers = await prisma.user.findMany();
     console.log(JSON.stringify(allUsers))
   
-    // // // First user, now project
-    await prisma.project.createMany({
-        data: projects
-    })
+    // // // // First user, now project
+    // await prisma.project.createMany({
+    //     data: projects
+    // })
   
     const allProjects = await prisma.project.findMany();
     console.log(JSON.stringify(allProjects))
   
-    // // // Now projectUser, so we need user uuid
-    await prisma.projectUser.createMany({
-        data: assignments
-    })
+    // // // // Now projectUser, so we need user uuid
+    // await prisma.projectUser.createMany({
+    //     data: assignments
+    // })
   
     const allAssignments = await prisma.projectUser.findMany();
     console.log(JSON.stringify(allAssignments))
